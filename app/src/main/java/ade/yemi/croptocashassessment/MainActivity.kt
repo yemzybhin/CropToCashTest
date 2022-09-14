e09e114ecb32e887517f043d64fcf908f00c7d57
yemzybhin <63170357+yemzybhin@users.noreply.github.com>
2022-09-14 15:46:23 +0100

    initial commit

---
+++ app/src/main/java/ade/yemi/croptocashassessment/MainActivity.kt
@@ -0,0 +1,11 @@
+package ade.yemi.croptocashassessment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
